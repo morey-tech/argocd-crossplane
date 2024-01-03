91fadf4e0305fb1b239174c8a6bf3baa90e7a71f
Nicholas Morey <nicholas@morey.tech>
2024-01-03 17:21:54 +0000

    fix(devct): remove reference to missing folder

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -11,6 +11,4 @@ kind export kubeconfig --name argocd-crossplane
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/kind/deploy.yaml
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s 
 
-kustomize build argocd/ | kubectl apply -f -
-
 echo "post-start complete" >> ~/status
