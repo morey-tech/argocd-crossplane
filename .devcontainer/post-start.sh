ec70c115ff37a0b68c34db2a28bec262d59a16a7
Nicholas Morey <nicholas@morey.tech>
2024-01-17 15:23:36 +0000

    fix(devct): remove ingress

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -7,8 +7,4 @@ echo "post-start start" >> ~/status
 # kind delete cluster --name argocd-crossplane
 kind export kubeconfig --name argocd-crossplane
 
-# configure ingress
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/kind/deploy.yaml
-kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s 
-
 echo "post-start complete" >> ~/status
